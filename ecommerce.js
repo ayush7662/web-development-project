c0a675b5e2277756e7d2b1bd8a927ee88d23e3b3
AYUSH RAJ <127328301+ayush7662@users.noreply.github.com>
2024-05-15 21:01:05 +0530

    first commit

---
+++ ecommerce.js
@@ -0,0 +1,13 @@
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
+
+
+
+Resources
